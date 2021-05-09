0d4780538fc81fd857145d16eb2aa1f7390d475e
jakub Marcinkowski <s21021@pjwstk.edu.pl>
2021-05-09 17:04:18 +0200

    book-1.7

---
+++ Zadania/src/main/java/podstawy/book/wejsciewyjscie1/Zadanie1_6.java
@@ -0,0 +1,11 @@
+package podstawy.book.wejsciewyjscie1;
+
+public class Zadanie1_6 {
+    public static void main (String[] args){
+        int a = 37;
+        int b = 11;
+
+        System.out.println ("reszta dzielenia :");
+        System.out.println (a%b);
+    }
+}
---
+++ Zadania/src/main/java/podstawy/book/wejsciewyjscie1/Zadanie1_7.java
@@ -0,0 +1,11 @@
+package podstawy.book.wejsciewyjscie1;
+
+import java.io.IOException;
+
+public class Zadanie1_7 {
+    public static void main(String[] args)
+    throws IOException {
+
+
+    }
+}
